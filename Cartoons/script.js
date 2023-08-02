9edeb651fc6e9e4e1af482a482790f666f85f705
Elyas <Elyas.shamal2@gmail.com>
2023-08-01 21:38:48 -0400

    add styles

--- Cartoons/script.js
+++ Cartoons/script.js
@@ -17,20 +17,33 @@ document.querySelectorAll(".nav-links").forEach((n) =>
 //end of humburger
 
 // fetch request to get the data
-document.addEventListener("click", () => {
+document.addEventListener("DOMContentLoaded", () => {
   fetch("https://api.sampleapis.com/cartoons/cartoons2D")
     .then((response) => response.json())
     .then((data) => data.forEach((element) => getData(element)));
 });
 
+// create the getData function
 const getData = (element) => {
   let card = document.createElement("div");
   card.classList.add("data");
 
   let img = document.createElement("img");
+  img.classList.add("image");
   img.src = element.image;
 
-  card.append(img);
+  let h3 = document.createElement("h3");
+  h3.classList.add("title");
+  h3.textContent = element.title;
+
+  let span = document.createElement("span");
+  span.textContent = "Title" + element.creator;
+
+  card.append(img, h3, span);
 
   document.getElementById("collection").appendChild(card);
 };
+
+fetch("https://api.sampleapis.com/cartoons/cartoons2D")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
